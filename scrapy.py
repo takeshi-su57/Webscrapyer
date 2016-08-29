3c59f24f10bb031fcf0cb68032ea40cc9f033c3e
hard-working-diamondcreator <lucas.lee.work61@gmail.com>
2016-08-29 18:32:26 -0400

    scrapy all the current relationship

--- scrapy.py
+++ scrapy.py
@@ -6,7 +6,6 @@ import pandas as pd
 from bs4 import BeautifulSoup 
 # read the website
 
-
 sampleUrl = ["http://www.whosdatedwho.com/dating/gregg-sulkin","http://www.whosdatedwho.com/dating/bella-thorne"]
 # htmlfile = urllib.urlopen('http://www.whosdatedwho.com/')
 # htmltext = htmlfile.read()
@@ -21,17 +20,25 @@ def get_relavant_relation(html):
 	### 1. get the relation
 	# print dom.by_tag
 	r = requests.get(html)
-	soup = BeautifulSoup(r.text)
+	soup = BeautifulSoup(r.text,"html.parser")
+
 	# bs.find_all("div",class= "ff-current-relationship.ff-has-readmore")
 	# print bsText.findall
 	# cssSoup = bs("<div class = ff-current-relationship.ff-has-readmore>")
 	# print soup.find_all("div")
 	### get the css soup
-	relations = soup.body.find_all("div",class_="ff-current-relationship")
+	relations = soup.body.find_all("p",class_="ff-auto-relationships")
 	print type(relations)
 	print "####\n"
-	print relations
 
+	### get the name of the relations
+	for i in relations:
+		for k in i.find_all("a")[1:]:
+			print k.get_text()
+
+
+	### output to json format
+		
 
 	# relation = []
 	# return relation
@@ -44,8 +51,6 @@ get_relavant_relation("http://www.whosdatedwho.com/dating/gregg-sulkin")
 # 	# titles = re.findall(pattern,htmltext)
 # 	print htmltext
 # structure data in dictionary format
-
-
 
 
 # print htmltext
