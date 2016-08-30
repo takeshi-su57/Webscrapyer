b0d87cc42818881779e0ad6f9cc46aea8cdbd972
hard-working-diamondcreator <lucas.lee.work61@gmail.com>
2016-08-30 00:01:54 -0400

    Prettified the output format

--- readme.md
+++ readme.md
@@ -3,5 +3,12 @@
 
 1 .scrapy data from website !http://www.whosdatedwho.com/
 2. structure data in key value(dict) format
+
+'''
+{'bella-thorne': [{'name': u'Gregg Sulkin', 'year': u' (2015 - 2016)'}, {'name': u'Ryan Nassif', 'year': u' (2015)'}, {'name': u'Brandon Thomas Lee', 'year': u' (2015)'}, {'name': u'Tristan Klier', 'year': u' (2012 - 2014)'}, {'name': u'Cody Simpson', 'year': u' (2011)'}, {'name': u'Garrett Backstrom', 'year': u' (2010 - 2011)'}, {'name': u'Jake T. Austin', 'year': u' (2010)'}]}
+'''
+
 3. display it as graph and edge 
  
+
+
--- scrapy.py
+++ scrapy.py
@@ -4,6 +4,7 @@ import requests
 from pattern import web
 import pandas as pd
 from bs4 import BeautifulSoup 
+import networkx as nx
 # read the website
 defaultUrl = "http://www.whosdatedwho.com/dating/"
 sampleUrl = ["http://www.whosdatedwho.com/dating/gregg-sulkin","http://www.whosdatedwho.com/dating/bella-thorne"]
@@ -24,15 +25,30 @@ def get_relationship(name):
 	link = defaultUrl+name
 	r = requests.get(link)
 	soup = BeautifulSoup(r.text,"html.parser")
-	relations = soup.body.find_all("p",class_="ff-auto-relationships")
+	relations = soup.body.find("p",class_="ff-auto-relationships")
 	resultSet = []
-	# for e in relations:
+	print relations.prettify()
+	item = relations.find("a")
+	resultSet.append({"name":item.get_text(),"year": item.next_sibling.get_text()})
+	while item.find_next_sibling("a") is not None :
+		item = item.find_next_sibling("a")
+		resultSet.append({"name":item.get_text(),"year": item.next_sibling.get_text()})
+	print resultSet	
+
+
+	# resultSet = []
+	# for e in relations:a[1].next_sibling.get_text()
 	# 	resultSet[e.a.get_text()] = {}
 	# for k in relations[0].find_all("a")[1:]:
 	# 	name = k.get_text().split()
 		# print "http://www.whosdatedwho.com/dating/"+name[0]+"-"+name[1]
-	for k in relations[0].find_all("a")[:]:
-		resultSet.append(k.get_text().split()[0]+"-"+k.get_text().split()[1])
+	# parse name to format xxx-x-xxx
+	# for k in relations.a[:]:
+	# 	p = re.split(r'[;,.\s]\s*',k.get_text())
+	# 	name = "-".join(p)
+	# 	resultSet.append({"name": name})
+
+
 
 	# print resultSet
 	return resultSet
@@ -79,6 +95,20 @@ def get_relavant_relation(html):
 	# return relation
 
 print type(get_relavant_relation("http://www.whosdatedwho.com/dating/Britt-Robertson"))
+g = nx.Graph()
+g.add_node("spam")
+g.add_edge(1,2)
+print(list(g.nodes()))
+print(list(g.edges()))
+
+
+#https://github.com/cs109/content/blob/master/HW5_solutions.ipynb
+def _color(s):
+    if '(R' in s:
+        return 'r'
+    if '(D' in s:
+        return 'b'
+    return 'k'
 
 # parse the relavent content
 # for i in range(0,2):
